1c2adb40df98d6aa1ea0658d4dcb9de1b17864b6
TazeenEhsan <tazeenehsan73@gmail.com>
2022-01-27 23:20:01 +0600

    last cmt

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ async function run() {
             const blogs = await cursor.toArray();
             res.send(blogs);
         });
+        // Get all blogs
+        app.get('/user/blogs', async (req, res) => {
+
+            console.log('hit');
+            const query = { status: 'Approved' };
+            const cursor = blogsCollection.find(query);
+            const blogs = await cursor.toArray();
+            res.send(blogs);
+        });
 
         // Get all users
         app.get('/users', async (req, res) => {
@@ -47,12 +56,28 @@ async function run() {
         // Get single product
         app.get('/blogs/:id', async (req, res) => {
             const id = req.params.id;
+            console.log('blog id', id);
             const query = { _id: ObjectId(id) };
             const user = await blogsCollection.findOne(query);
             // console.log('load user with id: ', id);
             res.send(user);
         });
 
+        // User admin or not
+        app.get('/users/:email', async (req, res) => {
+
+            console.log('admin checking');
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+                console.log(' admin hit and true');
+            }
+            res.json({ admin: isAdmin });
+        });
 
 
 
@@ -66,21 +91,57 @@ async function run() {
             res.json(result);
         });
 
+        //Post Single user
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            // console.log('got new user', newOrder);
+            // console.log('added user', result);
+            res.json(result);
+        });
+
 
         //UPDATE API ********************************************* Update**************************************
 
 
+
+
+        // Role change of user
+        app.put('/users/admin', async (req, res) => {
+            console.log('email pass er jnno put hit');
+            const user = req.body;
+            const filter = { email: user.email };
+            console.log(filter);
+            // const options = { upsert: true };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+        // Google sign er jnno
+        app.put('/users', async (req, res) => {
+
+            console.log(' googoler jnno put hit');
+            const user = req.body;
+            console.log(' googoler jnno put hit', user);
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
         app.put('/blogs/:id', async (req, res) => {
             const id = req.params.id;
-            const updatedOrder = req.body;
-            // console.log('updating req', updatedUser)
+
             const filter = { _id: ObjectId(id) };
+            console.log('updating blog', filter);
             const options = { upsert: true };
-            const updateDoc = { $set: { status: updatedOrder.orderstatus } };
-            const result = await ordersCollection.updateOne(filter, updateDoc, options)
+            const updateDoc = { $set: { status: 'Approved' } };
+            const result = await blogsCollection.updateOne(filter, updateDoc, options)
 
             res.json(result)
         });
+
 
 
         // // DELETE  API **************************************** Delete *************************************
@@ -105,6 +166,7 @@ app.get('/', (req, res) => {
 });
 app.get('/travel', (req, res) => {
     res.send('Good Morning Everyone');
+    console.log('object');
 });
 app.get('/hello', (req, res) => {
     res.send('Hello ST Blogs');
@@ -115,4 +177,4 @@ app.listen(port, () => {
     console.log('ST Blogs running at', port);
 });
 
-
+// sesh  
